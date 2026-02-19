789794a63da0aaea30da21469745e08e4be4a519
Mateus Ramos <contato.mateusramos@gmail.com>
2026-02-19 21:44:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.querySelectorAll('nav ul li a').forEach(link=>{
+  link.addEventListener('click',e=>{
+    e.preventDefault();
+    document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'});
+  });
+});
